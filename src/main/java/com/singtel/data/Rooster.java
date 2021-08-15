cf3d65848bca97e1db97174681cff7e1dfabe1a2
User <User@DESKTOP-CO6TR4F>
2021-08-15 20:15:08 +0800

    BONUS

--- src/main/java/com/singtel/data/Rooster.java
+++ src/main/java/com/singtel/data/Rooster.java
@@ -1,10 +1,51 @@
 package com.singtel.data;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Rooster extends Chicken {
+
+	static Map<String, String> lang;
+
+	static {
+		lang = new HashMap<String, String>();
+		lang.put("Danish", "kykyliky");
+		lang.put("Dutch", "kukeleku");
+		lang.put("Finnish", "kukko kiekuu");
+		lang.put("French", "cocorico");
+		lang.put("German", "kikeriki");
+		lang.put("Greek", "kikiriki");
+		lang.put("Hebrew", "coo- koo - ri-koo");
+		lang.put("Hungarian", "kukuriku");
+		lang.put("Italian", "chicchirichi");
+		lang.put("Japanese", "ko - ke - kok - ko -o");
+		lang.put("Portuguese", "cucurucu");
+		lang.put("Russian", "kukareku");
+		lang.put("Swedish", "kuckeliku");
+		lang.put("Turkish", "kuk-kurri-kuuu");
+		lang.put("Urdu", "kuklooku");
+		lang.put("Filipino", "tik talaok");
+	}
 
 	@Override
 	public boolean sing() {
 		System.out.println("Cock-a-doodle-doo");
 		return true;
+	}
+
+	public String sing(String language) {
+		String defLang = null;
+		if (null == lang || lang.isEmpty()) {
+			defLang = "Cock-a-doodle-doo";
+		} else {
+			defLang = lang.get(language);
+		}
+		System.out.println(defLang);
+		return defLang;
+	}
+	
+	public static void main(String[] args) {
+		Rooster roo = new Rooster();
+		roo.sing("Filipino");
 	}
 }
--- src/test/java/com/singtel/data/test/RoosterTest.java
+++ src/test/java/com/singtel/data/test/RoosterTest.java
@@ -3,6 +3,9 @@ package com.singtel.data.test;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -10,27 +13,56 @@ import com.singtel.data.Chicken;
 import com.singtel.data.Rooster;
 
 public class RoosterTest {
-	
+
 	Chicken rooster;
-	
+	Rooster roos;
+
 	@Before
 	public void setUp() {
 		rooster = new Rooster();
+		roos = new Rooster();
 	}
-	
+
 	@Test
 	public void testWalk() {
 		assertTrue(rooster.walk());
 	}
-	
+
 	@Test
 	public void testFly() {
 		assertFalse(rooster.fly());
 	}
-	
+
 	@Test
 	public void testSing() {
 		assertTrue(rooster.sing());
+	}
+
+	@Test
+	public void displayRoosterLang() {
+		List<String> lang = new ArrayList<String>();
+
+		lang.add("Danish");
+		lang.add("Dutch");
+		lang.add("Finnish");
+		lang.add("French");
+		lang.add("German");
+		lang.add("Greek");
+		lang.add("Hebrew");
+		lang.add("Hungarian");
+		lang.add("Italian");
+		lang.add("Japanese");
+		lang.add("Portuguese");
+		lang.add("Russian");
+		lang.add("Swedish");
+		lang.add("Turkish");
+		lang.add("Urdu");
+		lang.add("Filipino");
+
+		for (String language : lang) {
+			roos.sing(language);
+		}
+
 	}
 
 }
